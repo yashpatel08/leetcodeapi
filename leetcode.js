ea7b7015844abb319a2c5cc43c2fe4b59cd52480
Yash Patel <94280370+yashpatel08@users.noreply.github.com>
2024-07-10 22:00:41 +0530

    Update leetcode.js

--- leetcode.js
+++ leetcode.js
@@ -28,6 +28,22 @@ query getUserProfile($username: String!) {
             aboutMe
             starRating
         }
+        badges {
+            id
+            displayName
+            icon
+            creationDate
+        }
+        upcomingBadges {
+            name
+            icon
+        }
+        activeBadge {
+            id
+            displayName
+            icon
+            creationDate
+        }
         submitStats {
             totalSubmissionNum {
                 difficulty
@@ -51,56 +67,4 @@ query getUserProfile($username: String!) {
     }
 }`;
 
-
-
-// format data 
-const formatData = (data) => {
-    const allQuestions = data.allQuestionsCount;
-    const acSubmissionNum = data.matchedUser.submitStats.acSubmissionNum;
-    
-    let sendData = {
-        totalSolved: acSubmissionNum.reduce((acc, cur) => acc + cur.count, 0),
-        totalSubmissions: data.matchedUser.submitStats.totalSubmissionNum,
-        totalQuestions: allQuestions.reduce((acc, cur) => acc + cur.count, 0),
-        easySolved: acSubmissionNum.find(stat => stat.difficulty === 'easy')?.count || 0,
-        totalEasy: allQuestions.find(q => q.difficulty === 'easy')?.count || 0,
-        mediumSolved: acSubmissionNum.find(stat => stat.difficulty === 'medium')?.count || 0,
-        totalMedium: allQuestions.find(q => q.difficulty === 'medium')?.count || 0,
-        hardSolved: acSubmissionNum.find(stat => stat.difficulty === 'hard')?.count || 0,
-        totalHard: allQuestions.find(q => q.difficulty === 'hard')?.count || 0,
-        ranking: data.matchedUser.profile.ranking,
-        contributionPoint: data.matchedUser.contributions.points,
-        reputation: data.matchedUser.profile.reputation,
-        submissionCalendar: JSON.parse(data.matchedUser.submissionCalendar || '{}'),
-        recentSubmissions: data.recentSubmissionList,
-        badges: data.matchedUser.profile.badges || []
-    };
-    return sendData;
-}
-
-
-//fetching the data
-exports.leetcode = (req, res) => {
-    let user = req.params.id;
-    fetch('https://leetcode.com/graphql', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Referer': 'https://leetcode.com'
-        }, 
-        body: JSON.stringify({query: query, variables: {username: user}}),
-    
-    })
-    .then(result => result.json())
-    .then(data => {
-      if(data.errors){
-        res.send(data);
-      }else {
-        res.send(formatData(data.data));
-      }
-    })
-    .catch(err=>{
-        console.error('Error', err);
-        res.send(err);
-    });
-}
+export default query;
