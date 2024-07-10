c7caa9457d8b80616e05fb4b7d407da8bcd7353f
Yash Patel <94280370+yashpatel08@users.noreply.github.com>
2024-07-10 22:03:14 +0530

    Update leetcode.js

--- leetcode.js
+++ leetcode.js
@@ -1,70 +1,105 @@
-const query = `#graphql
-query getUserProfile($username: String!) {
+//graphql query
+const query = `
+  query getUserProfile($username: String!) {
     allQuestionsCount {
-        difficulty
-        count
+      difficulty
+      count
     }
     matchedUser(username: $username) {
-        username
-        githubUrl
-        twitterUrl
-        linkedinUrl
-        contributions {
-            points
-            questionCount
-            testcaseCount
+      contributions {
+        points
+      }
+      profile {
+        reputation
+        ranking
+      }
+      submissionCalendar
+      submitStats {
+        acSubmissionNum {
+          difficulty
+          count
+          submissions
         }
-        profile {
-            realName
-            userAvatar
-            birthday
-            ranking
-            reputation
-            websites
-            countryName
-            company
-            school
-            skillTags
-            aboutMe
-            starRating
+        totalSubmissionNum {
+          difficulty
+          count
+          submissions
         }
-        badges {
-            id
-            displayName
-            icon
-            creationDate
-        }
-        upcomingBadges {
-            name
-            icon
-        }
-        activeBadge {
-            id
-            displayName
-            icon
-            creationDate
-        }
-        submitStats {
-            totalSubmissionNum {
-                difficulty
-                count
-                submissions
-            }
-            acSubmissionNum {
-                difficulty
-                count
-                submissions
-            }
-        }
-        submissionCalendar
+      }
     }
-    recentSubmissionList(username: $username, limit: 20) {
-        title
-        titleSlug
-        timestamp
-        statusDisplay
-        lang
+    recentSubmissionList(username: $username) {
+      title
+      titleSlug
+      timestamp
+      statusDisplay
+      lang
+      __typename
     }
-}`;
+    matchedUserStats: matchedUser(username: $username) {
+      submitStats: submitStatsGlobal {
+        acSubmissionNum {
+          difficulty
+          count
+          submissions
+          __typename
+        }
+        totalSubmissionNum {
+          difficulty
+          count
+          submissions
+          __typename
+        }
+        __typename
+      }
+    }
+  }
+`;
 
-export default query;
+
+// format data 
+const formatData = (data) => {
+    let sendData =  {
+        totalSolved: data.matchedUser.submitStats.acSubmissionNum[0].count,
+        totalSubmissions:  data.matchedUser.submitStats.totalSubmissionNum,
+        totalQuestions: data.allQuestionsCount[0].count,
+        easySolved: data.matchedUser.submitStats.acSubmissionNum[1].count,
+        totalEasy: data.allQuestionsCount[1].count,
+        mediumSolved: data.matchedUser.submitStats.acSubmissionNum[2].count,
+        totalMedium: data.allQuestionsCount[2].count,
+        hardSolved: data.matchedUser.submitStats.acSubmissionNum[3].count,
+        totalHard: data.allQuestionsCount[3].count,
+        ranking: data.matchedUser.profile.ranking,
+        contributionPoint: data.matchedUser.contributions.points,
+        reputation: data.matchedUser.profile.reputation,
+        submissionCalendar: JSON.parse(data.matchedUser.submissionCalendar),
+        recentSubmissions: data.recentSubmissionList,
+        matchedUserStats: data.matchedUser.submitStats
+    }
+    return sendData;
+}
+
+//fetching the data
+exports.leetcode = (req, res) => {
+    let user = req.params.id;
+    fetch('https://leetcode.com/graphql', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Referer': 'https://leetcode.com'
+        }, 
+        body: JSON.stringify({query: query, variables: {username: user}}),
+    
+    })
+    .then(result => result.json())
+    .then(data => {
+      if(data.errors){
+        res.send(data);
+      }else {
+        res.send(formatData(data.data));
+      }
+    })
+    .catch(err=>{
+        console.error('Error', err);
+        res.send(err);
+    });
+}
